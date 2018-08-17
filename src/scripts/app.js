b05a3d0f68e8385b0f76a21cff15a21c31ebe7b2
Henrique Melanda <henrique.melanda@fcamara.com.br>
2018-08-17 11:47:07 -0300

    Example Line

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import './app.scss';
 
+/* CUBE
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
@@ -10,7 +10,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -26,3 +26,26 @@ const animate = () => {
 };
 
 animate();
+CUBE */
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+const scene = new THREE.Scene();
+
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+const geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
